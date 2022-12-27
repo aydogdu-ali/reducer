e2c26fec34e3ba8aa90bce7eb24296a0bae9793e
Ali <ali.ozgur@example.com>
2022-12-27 16:00:49 +0300

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import UseStateExample from './component/useStateExample';
 
 function App() {
   return (
     <div className="App">
       <h1>REDUCER KULLANIMI</h1>
-      
+      <UseStateExample/>
     </div>
   );
 }
---
+++ src/component/useStateExample.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+const useStateExample = () => {
+  // APIDEN VERİ ÇEKME DE KULLANACAĞIM STATLERİ TANIMLADIM.
+  const [userImage, setUserImage] = useState(); // GELEN RESMİ YAKALAMAK İÇİN
+  const [error, setError] = useState(""); // HATA MESAJI VERMEK İÇİN
+  const [loading, setLoading] = useState(false); // VERİ GELENE KADAR Kİ BUTONU PASİF HALE  GÖSTERMEK İÇİN
+
+  // APİDEM VERİYİ GETİRECEK FONKSİYONU TANIMLADIM.
+  const getUser = async () => {
+    const url = "https://randomuser.me/api";
+    setLoading(true);
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setUserImage(data.results[0]);
+      setError("");
+    } catch (error) {
+      setError("DATA CAN NOT BE FETCHED");
+      setUserImage("");
+      console.log(error);
+    }
+  };
+  console.log(error);
+  console.log(userImage);
+
+  return (
+    <div className="rounded ">
+      <div>
+        <button
+          onClick={getUser}
+          disabled={loading}
+          style={{ display: "block", margin: "1rem auto" }}
+        >
+          Get User Image
+        </button>
+        {error && <h2>{error}</h2>}
+        {userImage && (
+          <img width="15%" src={userImage.picture?.medium} alt="img" />
+        )}
+        {userImage && (
+          <p>
+            {" "}
+            {userImage.name.first} {userImage.name.last}
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default useStateExample;
